7974bbbed7f9e4664e0b72733ae938e67443c070
Yuchia <goldengoat233@gmail.com>
2026-03-03 23:29:05 +0100

    ...

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session, abort
 import sqlite3
-from flask_session import Session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, abort
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '14806762'
