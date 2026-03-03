47f1a17e456e47ee7a6ac4fb1f8a2e38a7100a06
Yuchia <goldengoat233@gmail.com>
2026-03-03 23:34:19 +0100

    ...

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '14806762'
 
 @app.route('/')
-def index():
-    return render_template("index.html")
-
-@app.route('/map')
 def map():
     return render_template("map.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
