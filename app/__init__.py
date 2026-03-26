bf9034a328c49ca3b5387b1fc91f9d3ad6322a51
Yuchia <goldengoat233@gmail.com>
2026-03-26 15:28:30 +0100

    。。。

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ def create_app():
     with app.app_context():
         # This is the magic line that reads models.py and creates the physical tables in Postgres
         db.create_all()
-        # 找到 create_app() 函数的末尾，把旧的 import routes 删掉，换成这下面两行：
+    
     from .routes import main_bp
     app.register_blueprint(main_bp)
 
