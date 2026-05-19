43d1588f0b630365b1aa7333955626705e34c2c8
Khushi <khushiii891@gmail.com>
2026-05-19 15:20:53 +0200

    limits

--- app/routes.py
+++ app/routes.py
@@ -240,7 +240,7 @@ def get_pesticides():
                     FROM pesticides_measurements
                     WHERE ST_Intersects(geometry, ST_MakeEnvelope(:w, :s, :e, :n, 4326))
                     GROUP BY meetpunt_code, wbhcode_omschrijving, jaar, geometry
-                    LIMIT 10000
+                    LIMIT 5000
                 ) agg
             ) features;
         """)
@@ -340,7 +340,7 @@ def get_schools():
                 ) AS feature
                 FROM schools
                 WHERE {filters}
-                LIMIT 10000
+                LIMIT 5000
             ) features;
         """)
         result = db.session.execute(sql_query, params).scalar()
